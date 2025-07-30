4b5c2ecf56179f9476e93f4e198df9a584501bbf
Thanis <mtmorion75@gmail.com>
2025-07-30 21:46:44 +0400

    Unit testing

---
+++ InsuranceAssesment/Insurance.Tests/PolicyTest.cs
@@ -0,0 +1,39 @@
+﻿using InsuranceAssesment.Data_Layer.Repository.IRepository;
+using InsuranceAssesment.Business_Layer.Services;
+using InsuranceAssesment.Data_Layer.Entities;
+using Xunit;
+using Moq;
+
+namespace Insurance.Tests
+{
+    public class PolicyTest
+    {
+        public class PolicyServiceTests
+        {
+            private readonly Mock<IPolicyRepository> _mockRepo;
+            private readonly PolicyService _service;
+
+            public PolicyServiceTests()
+            {
+                _mockRepo = new Mock<IPolicyRepository>();
+                var mockLogger = new Mock<ILogger<PolicyService>>();
+                _service = new PolicyService(_mockRepo.Object, mockLogger.Object);
+            }
+
+            [Fact]
+            public async Task GetAllAsync_ReturnsExpectedPolicies()
+            {
+                _mockRepo.Setup(x => x.GetAllAsync()).ReturnsAsync(new List<Policy>
+        {
+            new Policy { HolderName = "Alice", PolicyNumber = "P001" }
+        });
+
+                var result = await _service.GetAllAsync();
+
+                Assert.Single(result);
+                Assert.Equal("Alice", result.First().HolderName);
+            }
+        }
+
+    }
+}
